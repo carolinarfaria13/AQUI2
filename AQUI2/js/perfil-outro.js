1bd22a9f31692146a659f943e300400db11d4e65
marianapais <marianapais@ua.pt>
2026-06-13 00:05:44 +0100

    outro utilizador

---
+++ AQUI2/js/perfil-outro.js
@@ -0,0 +1,11 @@
+// Aguarda que a página carregue
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Página de Perfil de Inês carregada com sucesso.");
+});
+
+// Função para o botão "Ver mais"
+function verMais() {
+    console.log("Clicaste em Ver mais projetos!");
+    // Exemplo do que poderás fazer aqui futuramente:
+    // window.location.href = '../projetos/listadeprojetos.html';
+}
--- AQUI2/js/perfil.js
+++ AQUI2/js/perfil.js
@@ -1,9 +1,8 @@
-// Aguarda que o HTML seja todo carregado
+
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Página de perfil carregada e adaptada ao CSS base!");
 });
-
-// Função acionada ao clicar nos botões de ação laranja
+    §
 function acaoBotao(nomeDoBotao) {
     console.log("Clicaste no botão: " + nomeDoBotao);
     // Aqui poderás colocar a navegação. Exemplo:
