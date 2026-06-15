31e00ff7e1ff8104f2d844068ac060ca2a1af918
marianapais <marianapais@ua.pt>
2026-06-15 22:38:10 +0100

    php foto

--- pages/perfil/atualizar_foto.php
+++ pages/perfil/atualizar_foto.php
@@ -1,15 +1,13 @@
 <?php
-// Conexão à BD (ajusta conforme a tua configuração)
 $conn = new mysqli("localhost", "root", "", "nome_da_tua_bd");
 
 if(isset($_FILES['foto_perfil']) && $_FILES['foto_perfil']['error'] === 0) {
-    $pasta = "../../assets/uploads/"; // Cria esta pasta no teu projeto!
+    $pasta = "../../assets/uploads/";
     $nomeFicheiro = uniqid() . "_" . $_FILES['foto_perfil']['name'];
     $caminhoCompleto = $pasta . $nomeFicheiro;
 
     if(move_uploaded_file($_FILES['foto_perfil']['tmp_name'], $caminhoCompleto)) {
-        // Assume que o ID do utilizador está na sessão
-        $id_utilizador = 1; // Substituir por $_SESSION['id_utilizador']
+        $id_utilizador = 1;
 
         $sql = "UPDATE utilizadores SET foto_perfil = ? WHERE id = ?";
         $stmt = $conn->prepare($sql);
