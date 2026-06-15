dd2e5ca8fe8ea34fe03601d91945e88f27aecbd1
marianapais <marianapais@ua.pt>
2026-06-15 16:11:14 +0100

    barra dinamica

--- AQUI2/js/editarperfilinstituicao.js
+++ AQUI2/js/editarperfilinstituicao.js
@@ -39,3 +39,21 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var navbar = document.querySelector(".nav-fixav");
+
+    if (navbar) {
+        window.addEventListener("scroll", function() {
+
+            if (window.scrollY > 20) {
+                navbar.classList.add("nav-scrolled");
+            }
+            else {
+                navbar.classList.remove("nav-scrolled");
+            }
+
+        });
+    }
+});
--- AQUI2/js/perfil-outro.js
+++ AQUI2/js/perfil-outro.js
@@ -43,3 +43,21 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var navbar = document.querySelector(".nav-fixa");
+
+    if (navbar) {
+        window.addEventListener("scroll", function() {
+
+            if (window.scrollY > 20) {
+                navbar.classList.add("nav-scrolled");
+            }
+            else {
+                navbar.classList.remove("nav-scrolled");
+            }
+
+        });
+    }
+});
